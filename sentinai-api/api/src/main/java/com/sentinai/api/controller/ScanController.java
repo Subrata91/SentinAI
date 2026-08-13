93d176d71e45fd8fccc414c38bfbc6906cf6b866
Subrata91 <subrataron916@gmail.com>
2026-08-14 01:06:05 +0530

    Add CrossOrigin annotation to ScanController for Netlify CORS support

--- sentinai-api/api/src/main/java/com/sentinai/api/controller/ScanController.java
+++ sentinai-api/api/src/main/java/com/sentinai/api/controller/ScanController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/scan")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequiredArgsConstructor
 public class ScanController {
 
